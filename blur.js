3673fa365bea8347935aba988da74e3b6c17cbf3
mwakisaghu <harrisonchambela2016@gmail.com>
2021-08-09 16:19:31 +0300

    This shows how to blur the image/ image opacity

--- blur.js
+++ blur.js
@@ -12,5 +12,12 @@ function blurring() {
     clearInterval(interval);
   }
 
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = (100 - load) / 100;
+
+  blurValue = (30 * (100 - load)) / 100;
+  background.style.filter = `blur(${blurValue}px)`;
+  //background.style.filter = `blur(${scale(load, 0, 100, 30, 0)})px`;
+
   console.log(load);
 }
